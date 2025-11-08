50862bd1d727f7fc6e8e0e56a751161c4d7f33f3
spitarmy <y8jtz84wq5@privaterelay.appleid.com>
2025-11-08 17:55:29 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
