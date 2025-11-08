18b6cfd5a2e72dd0c1ede674c253cc23344b0128
spitarmy <y8jtz84wq5@privaterelay.appleid.com>
2025-11-08 19:26:37 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import UploadForm from './components/UploadForm';
 
 export default function Page() {
   return (
-    <main style={{ padding: 20 }}>
+    <main style={{ padding: 16 }}>
       <UploadForm />
     </main>
   );
